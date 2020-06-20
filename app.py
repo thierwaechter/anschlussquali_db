e4695f41c11466c9a83c32f9d19c4cc8d7a6d6f4
Chris <66311770+thierwaechter@users.noreply.github.com>
2020-06-20 15:42:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,7 @@ def hello():
 def f123467890():
     paragraph = "<p>Hallo Welt</p>"
     return "Hello Test!" + paragraph
+
+# These two lines should always be at the end of your app.py file.
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
