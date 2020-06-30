b6fe5c6ab5076bceec2769b008bc641d68667ea1
Chris <66311770+thierwaechter@users.noreply.github.com>
2020-06-30 18:52:44 +0200

    Update envconfig.sh

--- envconfig.sh
+++ envconfig.sh
@@ -7,7 +7,7 @@ mkdir -p /workspace/conda
 mkdir -p /workspace/data
 
 #Create a new env called arcw
-conda create --prefix /workspace/conda/arcw python=3.8 &&
+conda create --prefix /workspace/conda/arcw python=3.6 &&
 echo "conda activate /workspace/conda/arcw" >> ~/.bashrc &&
 export PATH=/workspace/conda/arcw/bin:$PATH &&
 source ~/.bashrc
