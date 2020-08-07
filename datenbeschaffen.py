815d8a8f48b282d0c8b39a7d59d3a5f9c2b696cf
Chris <christina.thierwaechter@gmail.com>
2020-08-07 20:50:27 +0000

    Zubringer und Abbringer Liste ok. Vergleich ungetestet

--- datenbeschaffen.py
+++ datenbeschaffen.py
@@ -1,5 +1,5 @@
 import csv
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def abbringer(linien_text_abbringer, abbringer_ort):
     print("Funktion Abbringer wird ausgeführt")
@@ -9,9 +9,13 @@ def abbringer(linien_text_abbringer, abbringer_ort):
     
         for line in linereader:
             if line[7] == linien_text_abbringer and line[13] == abbringer_ort:
-                ab_prognose_str = line[17]
-                abbringer_zeiten.append(ab_prognose_str)
-        return ab_prognose_str
+                ab_prognose_str = line[18]
+                if ab_prognose_str:
+                    ab_prognose = datetime.strptime(ab_prognose_str, "%d.%m.%Y %H:%M:%S")
+                    abbringer_zeiten.append(ab_prognose)
+                else:
+                    pass
+        return abbringer_zeiten
 
 def zubringer(linien_text_zubringer, zubringer_ort):
     print("Funktion Zubringer wird ausgeführt")
@@ -22,5 +26,9 @@ def zubringer(linien_text_zubringer, zubringer_ort):
         for line in linereader:
             if line[7] == linien_text_zubringer and line[13] == zubringer_ort:
                 an_prognose_str = line[15]
-                zubringer_zeiten.append(an_prognose_str)
+                if an_prognose_str:
+                    an_prognose = datetime.strptime(an_prognose_str, "%d.%m.%Y %H:%M:%S")
+                    zubringer_zeiten.append(an_prognose)
+                else:
+                    pass
         return zubringer_zeiten
--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 from datenbeschaffen import abbringer, zubringer
+from datetime import datetime, timedelta
 
 #Zubringer Angaben
 linien_text_zubringer = 'S1'
@@ -9,11 +10,29 @@ linien_text_abbringer = '160'
 abbringer_ort = 'Münsingen Bahnhof'
 
 #Umsteigezeit
-max_umsteigezeit: 360 #in Sekunden
+min_umsteigezeit = timedelta(minutes=2)
+max_umsteigezeit = timedelta(minutes=6)
 
+#an_inkl_max = [] # Die Abbringerzeit inklusive der maximalen Umsteigezeit
 auswertung = {}
 
-#liste_zubringer_zeiten = zubringer(linien_text_zubringer, zubringer_ort)
+liste_zubringer_zeiten = zubringer(linien_text_zubringer, zubringer_ort)
+print(liste_zubringer_zeiten)
 liste_abbringer_zeiten = abbringer(linien_text_abbringer, abbringer_ort)
-#print(liste_zubringer_zeiten)
 print(liste_abbringer_zeiten)
+
+# Zur Zubringerzeit wird die maximale Umseigezeit gerechnet. Dank der Umwandlung funtioniert die 
+# Rechnung auch um Mittarnacht
+#for zeit_str in liste_zubringer_zeiten:
+#    zeit = datetime.strptime(zeit_str, "%d.%m.%Y %H:%M:%S")
+#    max_zeit = zeit + max_umsteigezeit
+#    an_inkl_max.append(max_zeit)
+
+# Mit dieser neuen Zeit wird ein Anschluss beim Abbringer gesucht. Die Rückgabe ist
+# "Anschluss vorhanden" oder "Kein Anschluss gefunden" inkl. der Zugeordneten Abbringerzeit.
+
+for an_zeit in liste_zubringer_zeiten:
+    for ab_zeit in liste_abbringer_zeiten:
+        if ab_zeit > an_zeit + min_umsteigezeit and ab_zeit < an_zeit + max_umsteigezeit:
+            print('Anschluss OK ' + str(an_zeit) + ' und ' + str(ab_zeit) + ' passen.')
+            break
